ad72b5a017746e40424478f94401b46d08862e93
Anh Quang <anhta1234321@gmail.com>
2023-09-19 23:33:06 +0700

    test code

--- font-end/src/Pages/login/login.jsx
+++ font-end/src/Pages/login/login.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import "./login.scss";
 
-const Login =()=>{
-    return(
-        <div className="login">
-            Login
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello world.</h1>
+          <p>
+            Có một số lý do khiến bạn có thể gặp phải vấn đề này. Dưới đây là
+            một số điều bạn có thể kiểm tra: Bạn đã cài đặt phần mở rộng JSX
+            Snippets đúng cách chưa? Bạn có thể kiểm tra bằng cách mở Visual
+          </p>
+          <span>Bạn không có tài khoản ? </span>
+          <button>Đăng kí </button>
         </div>
-    )
-}
+        <div className="right">
+          <h1>Đăng nhập</h1>
+          <form action="">
+            <input type="text" placeholder="Tên đăng nhập" />
+            <input type="password" placeholder="Mật khẩu " />
+            <button>Đăng nhập</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
